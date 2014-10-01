189cdb7cba2ac01ed16631ce62629d58ed2ad9a8
kybu <kybu@kybu.org>
2014-10-01 15:52:57 +0100

    Application directory wasn't correctly initialised.

--- lib/globals.rb
+++ lib/globals.rb
@@ -1,4 +1,9 @@
 require_relative 'helpers'
 
+require 'fileutils'
+
 APPDATA = (Helpers.appData+'/kybu/components').gsub '\\', '/'
 
+unless File.directory? APPDATA
+  FileUtils.mkdir_p APPDATA
+end
